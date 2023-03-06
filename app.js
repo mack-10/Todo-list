55dde077f89341081f24a562afc763bb08f5d287
Sujin Jeon <mackerel0106@gmail.com>
2023-03-06 18:42:19 +0900

    Edit app.js: DELETE 기능 추가

--- app.js
+++ app.js
@@ -34,18 +34,6 @@ app.use("/static", express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 
 
-// GET METHOD: 서버 자원을 가져오고자 할 때 사용.
-// READ
-app.get("/", (req, res) => {
-	TodoTask.find({}) // find all documents
-		.then((tasks) => {
-			res.render("todo.ejs", { todoTasks: tasks });
-		})
-		.catch ((err) => {
-			console.log(err);
-		})
-});
-
 // POST METHOD: 서버에 자원을 새로 등록하고자 할 때 사용.
 // CREATE
 app.post("/", (req, res) => {
@@ -59,6 +47,63 @@ app.post("/", (req, res) => {
 		})
 		.catch ((err) => {
 			res.redirect("/");
-		})
+		});
 });
 
+
+// GET METHOD: 서버 자원을 가져오고자 할 때 사용.
+// READ
+app.get("/", (req, res) => {
+	TodoTask.find({}) // find all documents
+		.then((tasks) => {
+			res.render("todo.ejs", { todoTasks: tasks });
+		})
+		.catch ((err) => {
+			console.log(err);
+		});
+});
+
+
+
+// UPDATE
+app
+	.route("/edit/:id")
+	.get((req, res) => {
+		const id = req.params.id;
+
+		TodoTask.find({})
+			.then((tasks) => {
+				res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+			})
+			.catch ((err) => {
+				console.log(err);
+			});
+	})
+	.post ((req, res) => {
+		const id = req.params.id;
+
+		TodoTask.findByIdAndUpdate(id, { content: req.body.content })
+			.catch ((err) => {
+				if (err)
+					return res.send(500, err);
+				res.redirect("/");
+			});
+	});
+
+
+// DELETE
+app
+	.route("/remove/:id")
+	.get ((req, res) => {
+		const id = req.params.id;
+
+		TodoTask.findByIdAndDelete(id)
+			.then (() => {
+				res.redirect("/");
+			})
+			.catch ((err) => {
+				if (err)
+					return res.send(500, err);
+				res.redirect("/");
+			});
+	});
