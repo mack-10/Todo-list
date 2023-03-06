9d63cf4632ca7f176930bb78496b1b961061b29e
Sujin Jeon <mackerel0106@gmail.com>
2023-03-06 16:23:45 +0900

    Add TodoTask.js: Make schema, add Schema in app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import path from "path";
+import { fileURLToPath } from "url";
+import TodoTask from "./models/TodoTask";
 
 const app = express();
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
@@ -22,14 +28,18 @@ mongoose
 
 
 // VIEW ENGINE CONFIGURATION
-app.use("/static", express.static("public"));
+// 경로는 절대경로로 해주는게 좋다.
+// app.use("/static", express.static(__dirname + "/public"));
+app.use("/static", express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 
+
+// GET METHOD: 서버 자원을 가져오고자 할 때 사용.
 app.get("/", (req, res) => {
 	res.render('todo.ejs');
 })
 
-// POST METHOD
+// POST METHOD: 서버에 자원을 새로 등록하고자 할 때 사용.
 // When there's post signal, show the request's body
 app.post("/", (req, res) => {
 	console.log(req.body);
